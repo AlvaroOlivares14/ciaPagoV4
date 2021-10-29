13048bff10309de532b9920c08a4030a1e70501f
alvaro <alvaro.olivares@alumnos.uda.cl>
2021-10-28 22:40:07 -0300

    version 2° sprint inicio (lista de cuotas a la mitad)

---
+++ djangoPrueba/ciaPago/ciaPago/urls.py
@@ -0,0 +1,38 @@
+"""ciaPago URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, reverse_lazy
+from ciaPago.views import index, perfil
+from django.contrib.auth import views
+from django.contrib.auth.decorators import login_required
+from django.conf.urls import include
+from django.http import HttpResponse
+from django.conf.urls import url
+from django.views.generic.base import TemplateView
+from django.views.generic.base import RedirectView
+
+urlpatterns = [
+	#url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+	url(r'^$', RedirectView.as_view(url='index/login/', permanent=False)),
+	path('index/login/', views.LoginView.as_view(template_name="index.html"), name='index'),
+	#url(r'^accounts/profile/$', login_required(perfil), login_url='/index/login/'), name='summary'),
+	path('accounts/profile/', perfil, name='perfil'),
+	path('admin/', admin.site.urls),
+    path('index/', include('django.contrib.auth.urls')),
+
+    
+    path('logout/', views.LogoutView.as_view(next_page=reverse_lazy('index')), name='logout')
+]
---
+++ djangoPrueba/ciaPago/ciaPago/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse
+from django.template import Template, context
+from django.template.loader import get_template
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from django.template import RequestContext
+from ciaPago.basePrueba import Cuotassociales
+#import os
+
+#pagina = 
+
+def index(request):
+	pagina = get_template('index.html')
+	p2 = pagina.render()
+	return HttpResponse(p2)
+
+
+@login_required(login_url='/index/login/')
+def perfil(request):
+	#username = request.user.username
+	cuotas = Cuotassociales.objects.all()
+	data = {
+		'cuotas': cuotas
+	}
+	return render(request, 'perfil.html', data)
+
+	#pagina = get_template('perfil.html')
+	#p2 = pagina.render()
+	#return HttpResponse(p2)
+
+"""def login(request):
+	#pagina = get_template('index.html')
+	#p1 = pagina.render()
+	#return HttpResponse(p1)
+"""
+#def test1 (request):
+	#mypath = os.getcwd()
+	#maindir = os.path.dirname(os.path.dirname(mypath))
+	#indexdir = maindir + '\\SistemaWebElectivo5\\index.html'
+	#os.chdir(indexdir)
+	#os.getcwd()
+	#return HttpResponse()
